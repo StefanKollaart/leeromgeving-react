191f738ad09769e93538d9eeb609a875ce77d9df
StefanKollaart <garbera@icloud.com>
2017-03-15 11:42:53 +0100

    Checkbox logic

--- src/admin/users/EditUser.js
+++ src/admin/users/EditUser.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux'
 import editUser from '../../actions/user/update'
 import fetchUsers from '../../actions/user/fetch'
 import fetchGroups from '../../actions/groups/fetch'
+import RenderCheckboxes from './RenderCheckboxes'
 
 export class EditUser extends PureComponent {
   componentWillMount() {
     this.props.fetchUsers()
     this.props.fetchGroups()
-    this.createCheckboxes = this.createCheckboxes.bind(this)
     this.handleFirstName = this.handleFirstName.bind(this)
     this.handleLastName = this.handleLastName.bind(this)
     this.handleEmail = this.handleEmail.bind(this)
+    this.renderCheckboxes = this.renderCheckboxes.bind(this)
   }
 
   loadEditUser() {
@@ -20,8 +21,11 @@ export class EditUser extends PureComponent {
         first_name: this.props.first_name,
         last_name: this.props.last_name,
         email: this.props.email,
-        groups: this.props.groups,
       }
+  }
+
+  renderCheckboxes(checkbox, index) {
+    return <RenderCheckboxes key={index} checkBoxId={index} userGroups={this.props.groups} {...checkbox} />
   }
 
   handleFirstName(event) {
@@ -53,55 +57,6 @@ export class EditUser extends PureComponent {
       this.props.editUser(user)
   }
 
-  createCheckboxes() {
-    if(this.props.allGroups.length >= 1) {
-      return this.props.allGroups.map(function(group, index) {
-        function courseName(courseId) {
-          switch (courseId) {
-          case "1":
-            return "Pedicure"
-            break;
-          case "2":
-            return "Sportmassage"
-            break;
-          default:
-            return "Error"
-            break;
-          }
-        }
-        function courseDay(courseDayId) {
-          switch (courseDayId) {
-          case "0":
-            return "Zondag"
-            break;
-          case "1":
-            return "Maandag"
-            break;
-          case "2":
-            return "Dinsdag"
-            break;
-          case "3":
-            return "Woensdag"
-            break;
-          case "4":
-            return "Donderdag"
-            break;
-          case "5":
-            return "Vrijdag"
-            break;
-          case "6":
-            return "Zaterdag"
-            break;
-          default:
-            return "Error"
-            break;
-          }
-        }
-        return(<span key={index}><input type="checkbox" value={group._id} key={index}/>{courseName(group.courseId)} - {group.year} - {courseDay(group.day)}</span>)
-      })
-    }
-  }
-
   render() {
     if(this.props.email != undefined) {
       {this.loadEditUser()}
@@ -119,7 +74,7 @@ export class EditUser extends PureComponent {
             <div className="input">
               <input type="text" placeholder="E-mail" value={this.state.email} onChange={this.handleEmail}/>
             </div>
-            {this.createCheckboxes()}
+            {this.props.allGroups.map(this.renderCheckboxes)}
             <input type="submit" value="Aanmaken" />
           </form>
         </div>
@@ -130,6 +85,7 @@ export class EditUser extends PureComponent {
       )
     }
   }
+  debugger
 }
 
 const mapStateToProps = ({ users, groups }, { params }) => {
---
+++ src/admin/users/RenderCheckboxes.js
@@ -0,0 +1,88 @@
+import React, {PureComponent, PropTypes} from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+
+class RenderCheckboxes extends PureComponent {
+  constructor(props) {
+    super(props)
+    debugger
+    for(var i = 0; i < this.props.userGroups.length; i++) {
+      if(this.props.userGroups[i].courseId == this.props.courseId && this.props.userGroups[i].year == this.props.year && this.props.userGroups[i].day == this.props.day) {
+        this.state = {
+          isChecked: true,
+        }
+      } else {
+        this.state = {
+          isChecked: false,
+        }
+      }
+    }
+  }
+
+  componentWillMount() {
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    this.setState({
+      isChecked: !this.state.isChecked
+    })
+  }
+
+  renderDay(day) {
+    switch (day) {
+      case 0:
+        return "Zondag"
+        break;
+      case 1:
+        return "Maandag"
+        break;
+      case 2:
+        return "Dinsdag"
+        break;
+      case 3:
+        return "Woensdag"
+        break;
+      case 4:
+        return "Donderdag"
+        break;
+      case 5:
+        return "Vrijdag"
+        break;
+      case 6:
+        return "Zaterdag"
+        break;
+
+      default:
+      return "Dag niet gevonden"
+    }
+  }
+
+  renderCourse(courseId) {
+    switch (courseId) {
+      case 1:
+        return "Pedicure"
+        break;
+      case 2:
+        return "Sportmassage"
+        break;
+      default:
+        return "Course niet gevonden"
+    }
+  }
+
+  static propTypes = {
+    courseId: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    day: PropTypes.number.isRequired,
+    userGroups: PropTypes.array,
+  }
+
+  render() {
+    const { courseId, year, day, checkBoxId } = this.props
+
+    return(<div><input type="checkbox" checked={this.state.isChecked} onChange={this.handleClick} />{this.renderCourse(courseId)} - {year} - {this.renderDay(day)}</div>)
+  }
+}
+
+export default RenderCheckboxes
